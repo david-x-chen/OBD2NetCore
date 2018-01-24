0dcb0ad889d60d63a9d0c9d1a7ff3f984df9fae2
David Chen <davidchen@d-techpro.net>
2018-01-24 14:35:10 +0800

    Added AvaloniaUI application to support cross-platform applcation.

---
+++ src/Obd2NetCore.App/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Obd2NetCore.App
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
